86a376c2461685e175d35cc9aede7fb4a7c8dbe5
Ke Jia <ke.jia@metropolia.fi>
2024-10-03 02:20:25 +0300

    Add an New API without auth

---
+++ backend-no-auth/models/jobModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  type: { type: String, required: true },
+  description: { type: String, required: true },
+  company: {
+    name: { type: String, required: true },
+    contactEmail: { type: String, required: true },
+    contactPhone: { type: String, required: true }
+  },
+  location: { type: String, required: true }, // e.g., City, State, or Remote
+  salary: { type: Number, required: true }, // e.g., Annual or hourly salary
+  postedDate: { type: Date, default: Date.now }, // Date the job was posted
+  status: { type: String, enum: ['open', 'closed'], default: 'open' }, // Job status
+});
+
+ 
+//add  virtual field id
+jobSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    return ret;
+  }
+});
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
+
