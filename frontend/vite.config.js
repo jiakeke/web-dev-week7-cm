07b722cad6b6aafdea41ac415e94e9bc48a7895e
Ke Jia <ke.jia@metropolia.fi>
2024-10-03 12:13:33 +0300

    add .env for fronted with auth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: parseInt(process.env.VITE_PORT) || 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:4000',
         changeOrigin: true,
       },
     }
